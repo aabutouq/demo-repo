6251a35a26f254aabbbeb88a17fa547e5ef8ee1c
aabutouq <58777389+aabutouq@users.noreply.github.com>
2019-12-16 15:51:21 +0200

    Create Constructor

--- FirstProject/src/Login/Login.java
+++ FirstProject/src/Login/Login.java
@@ -1,13 +1,37 @@
 package Login;
 
+
+
 public class Login {
+	
+	private int var;
+	
+	public Login (int param) {
+		
+	var=param;
+	}
+	
+	public void ReturnVlaue () {
+		System.out.println(var);
+	
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Login Obj = new Login(10);
+	    Obj.ReturnVlaue();
+	    
+		
+	
+		
+		
 
-		System.out.println("test");
+		/*System.out.println("test");
 		System.out.println("test from new branch");
-		System.out.println("test from new");
+		System.out.println("test from new");*/
+		
+	
 	}
 
 }
